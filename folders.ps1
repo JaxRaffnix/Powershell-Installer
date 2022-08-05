27b19390d1b80ff0d60992885e8062f7bfc49c0f
JaxRaffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-08-05 13:54:20 +0200

    added github repos to installer

--- folders.ps1
+++ folders.ps1
@@ -4,3 +4,7 @@ New-Item "$HOME\Coding" -ItemType Directory
 
 New-Item "$HOME\Workspace" -ItemType Directory
 
+cd $HOME\Workspace\
+New-Item "$HOME\Workspace\Temp" -ItemType Directory
+
+cd $HOME
--- installer.ps1
+++ installer.ps1
@@ -1,19 +1,30 @@
-# Create user folders
+# Create User Folders
 .\folders.ps1
 
-# Install scoop
+# Install Scoop
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
 irm get.scoop.sh | iex
 
-# Install software with scoop
+# Install Software with Scoop
 .\scoop-apps.ps1
 
 scoop update *
 
-# Create link from scoop apps to Apps User Folder
+# Create Link for Scoop Apps to User Folder
 sudo New-Item -ItemType SymbolicLink -Path $HOME\AppT\ -Name Scoop-Apps -Target $HOME\scoop\apps
 
-# 
+# Add Github Repositories to User Folder
+cd $HOME\Coding\
+
+git clone https://github.com/JaxRaffnix/Hilfestellung.git
+
+git clone https://github.com/JaxRaffnix/Powershell-Backups.git
+
+git clone https://github.com/JaxRaffnix/Powershell-Terminal-Settings.git
+cd .\Powershell-Terminal-Settings\
+.\apply_settings.ps1
+
+cd $HOME
 
 # configure software
 code
