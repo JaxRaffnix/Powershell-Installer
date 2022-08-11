50e4b993fdedae1ac2e2fe22dd4b5898b99063c4
JaxRaffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-08-11 13:19:59 +0200

    added nvidia website

--- installer.ps1
+++ installer.ps1
@@ -12,20 +12,19 @@
     .\scoop-apps.ps1
 
 # Create Link in Aoo Folder to  Scoop Apps
-    sudo New-Item -ItemType SymbolicLink -Path $HOME\AppT\ -Name Scoop-Apps -Target $HOME\scoop\apps
+    sudo New-Item -ItemType SymvolicLink -Path $HOME\Apps\ -Name Scoop-Apps -Target $HOME\scoop\apps
 
 # Add Github Repositories to User Folder
-cd $HOME\Coding\
+    Set-Location $HOME\Coding\
 
-git clone https://github.com/JaxRaffnix/Hilfestellung.git
+    git clone https://github.com/JaxRaffnix/Hilfestellung.git
+    git clone https://github.com/JaxRaffnix/Powershell-Backups.git
+    git clone https://github.com/JaxRaffnix/Powershell-Terminal-Settings.git
 
-git clone https://github.com/JaxRaffnix/Powershell-Backups.git
+    Set-Location .\Powershell-Terminal-Settings\
+    .\apply_settings.ps1
 
-git clone https://github.com/JaxRaffnix/Powershell-Terminal-Settings.git
-cd .\Powershell-Terminal-Settings\
-.\apply_settings.ps1
-
-cd $HOME
+Set-Location $HOME
 
 # configure software
 code
--- scoop-apps.ps1
+++ scoop-apps.ps1
@@ -1,5 +1,5 @@
 # Include buckets 
-    scoop bucket add extras versions nonportable
+    scoop bucket add extras versions
     scoop bucket add mybucket https://github.com/JaxRaffnix/Powershell-scoop-bucket.git
 
 # Coding
@@ -7,13 +7,12 @@
 
 # Helpers
     scoop install keepass 7zip treesize-free restic sudo
-    sudo scoop install nvidia-display-driver-with-3d-vision-np
 
 # Programming Languages
     scopp install python perl miktex msys2
 
 # Communication
-    scoop install thunderbird whatsapp signal microsoft-teams zoom discord
+    scoop install thunderbird whatsapp signal zoom discord
 
 # Office
     scoop install googlechrome nextcloud spotify aimp
@@ -28,6 +27,7 @@
     scoop update *
 
 # Manual Downloads
+    Start-Process https://www.nvidia.de/Download/index.aspx?lang=de
     Start-Process https://e3.boxcdn.net/box-installers/desktop/releases/win/Box-x64.msi
     Start-Process https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
     Start-Process https://tools.pdf24.org/de/creator
