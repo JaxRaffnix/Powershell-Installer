e066050ac43e49252ae09e1208d3047267cbe1a5
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-07-13 13:51:46 +0200

    updated apps

--- scoop-apps.ps1
+++ scoop-apps.ps1
@@ -1,6 +1,6 @@
 # Include buckets 
     scoop bucket add extras
-    scoop bucket add mybucket https://github.com/JaxRaffnix/Powershell-scoop-bucket.git
+    # scoop bucket add mybucket https://github.com/JaxRaffnix/Powershell-scoop-bucket.git
     scoop bucket add games
 
 # Coding
@@ -11,7 +11,7 @@
     reg import $HOME\scoop\apps\windows-terminal\current\install-context.reg
 
 # Helpers
-    scoop install keepass 7zip treesize-free restic sudo innounp dark
+    scoop install keepass 7zip restic sudo innounp dark
 
     # sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
 
@@ -19,8 +19,6 @@
     scoop install python perl miktex gcc
 
     reg import $HOME\scoop\apps\python\current\install-pep-514.reg
-
-    # Write-Output "run msys2, then 'pacman -S mingw-w64-x86_64-gcc'"
 
 # Communication
     scoop install thunderbird whatsapp signal zoom discord
@@ -32,8 +30,6 @@
 
 # Games
     scoop install steam ubisoftconnect epic-games-launcher playnite 
-
-    # Write-Output "!Change Steam Download Folder!"
 
 # MyBucket
     # scoop install 
@@ -49,14 +45,14 @@
 # Manual Downloads
     Start-Process https://www.nvidia.de/Download/index.aspx?lang=de
     Start-Process https://e3.boxcdn.net/box-installers/desktop/releases/win/Box-x64.msi
-    Start-Process https://tools.pdf24.org/de/creator
+    # Start-Process https://tools.pdf24.org/de/creator
     Start-Process https://www.office.com/?auth=2
     Start-Process https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe
     Start-Process https://www.spotify.com/de/download/other/
     start-process https://visualstudio.microsoft.com/de/visual-cpp-build-tools/
 
 # Knows Bugs
-    # spotify, if OS is not in english
+    # spotify wont update, if OS language is ever changed on the machine
 
 # Not Sure
-    # battle.net, kodi, adobe reader, epson, hadinet printer, aimp
+    # battle.net, kodi, hadinet printer
